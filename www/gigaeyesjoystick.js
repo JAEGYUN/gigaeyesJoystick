96288fb8a0be78adab78a1fe757f0645dd7209e7
civan <civan.blaze@gmail.com>
2017-07-11 12:32:11 +0900

    remove move

--- www/gigaeyesjoystick.js
+++ www/gigaeyesjoystick.js
@@ -3,15 +3,3 @@ var exec = require('cordova/exec');
 exports.watch = function(moviePath, success, error) {
     exec(success, error, "GigaeyesJoystick", "watch", [moviePath]);
 };
-
-exports.move = function(){
-    var callbackSuccess = function(result){
-        alert(result);
-    }
-
-    var callbackFail = function(err){
-        alert(err);
-    }
-
-    exec(callbackSuccess, callbackFail,"GigaeyesJoystick","move",[]);
-}
